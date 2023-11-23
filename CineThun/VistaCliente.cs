f0a4747ab269a9ae38ee452d1402763b3f682412
FranciscoH27 <franuleg@gmail.com>
2023-11-23 17:59:34 -0300

    Entrega parcial

--- CineThun/VistaCliente.cs
+++ CineThun/VistaCliente.cs
@@ -106,5 +106,10 @@ namespace CineThun
             hiloCambiarPeliculas.Abort();
             base.Close();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
